65245891919e4ffb18d9d1781254ea48a9f1d3f0
Sam Guliker <sam.guliker@studioregale.com>
2021-05-05 15:38:30 +0200

    Boilerplate reset

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,6 @@ export default function Header({setUserInput, userInput, header}) {
     return (
         <header className="header">
             <h1 className="heading-01">{header}</h1>
-            <input 
-                    className="searchbar" 
-                    value={userInput} 
-                    type="text" 
-                    name="search" 
-                    onChange={e => {setUserInput(e.target.value)}} 
-                    placeholder="Search a currency"
-            />
-            <Search className="search-icon" />
         </header>
     )
 }
